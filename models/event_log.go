c6a4954a2a33ca228d2d14f7654754a22abbda16
aabb <809073023@qq.com>
2026-01-04 14:39:50 +0800

    feat: add replayRange logic

---
+++ models/event_log.go
@@ -0,0 +1,19 @@
+package models
+
+import "time"
+
+type EventLog struct {
+	ID          uint
+	TxHash      string
+	LogIndex    uint
+	BlockNumber uint64
+	Event       string
+	Contract    string
+	Indexed     string
+	Data        string
+	CreatedAt   time.Time
+}
+
+func (EventLog) TableName() string {
+	return "event_log"
+}
--- service/listener.go
+++ service/listener.go
@@ -2,12 +2,17 @@ package service
 
 import (
 	"context"
+	"encoding/json"
 	"errors"
+	staking "go-solidity-staking/gen"
 	"go-solidity-staking/models"
+	"math/big"
 	"strings"
 	"time"
 
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/ethclient"
 	"gorm.io/gorm"
 )
@@ -46,11 +51,74 @@ func (l *listenerService) ReplayFromLast(ctx context.Context, contractAddress co
 		return nil
 	}
 	// 回放
-	return nil
+	return l.replayRange(ctx, contractAddress, lastBlock+1, latest)
 }
 
 func (l *listenerService) StartReplayLoop(ctx context.Context, contractAddress common.Address, starkBlock uint64, confirmations uint64, interval time.Duration) {
 
+}
+
+func (l *listenerService) replayRange(ctx context.Context, contractAddress common.Address, start uint64, end uint64) error {
+	s, err := staking.NewStaking(contractAddress, l.client)
+	if err != nil {
+		return err
+	}
+	endCopy := end
+	stakedIter, err := s.FilterStaked(&bind.FilterOpts{Start: start, End: &endCopy, Context: ctx}, nil, nil)
+	if err != nil {
+		return err
+	}
+	defer stakedIter.Close()
+	for stakedIter.Next() {
+		l.handleStaked(stakedIter.Event)
+	}
+	return l.setSyncBlock(syncKey(contractAddress), end)
+}
+func (l *listenerService) handleStaked(ev *staking.StakingStaked) {
+	if ev == nil {
+		return
+	}
+	ok, err := l.recordEvent(ev.Raw, "staked")
+	if err != nil || !ok {
+		return
+	}
+	// 更新区块高度
+	_ = l.setSyncBlock(syncKey(ev.Raw.Address), ev.Raw.BlockNumber)
+}
+func (l *listenerService) recordEvent(logEntry types.Log, eventName string) (bool, error) {
+	if len(logEntry.Topics) < 3 {
+		return false, nil
+	}
+	signature := logEntry.Topics[0].Hex()
+	address := common.BytesToAddress(logEntry.Topics[1].Bytes()[12:])
+	amount := new(big.Int).SetBytes(logEntry.Topics[2].Bytes())
+	indexedMap := map[string]string{
+		"signature": signature,
+		"user":      address.Hex(),
+		"amount":    amount.String(),
+	}
+	marshal, err := json.Marshal(indexedMap)
+	if err != nil {
+		return false, err
+	}
+	entry := models.EventLog{
+		TxHash:      logEntry.TxHash.Hex(),
+		LogIndex:    logEntry.Index,
+		BlockNumber: logEntry.BlockNumber,
+		Event:       eventName,
+		Indexed:     string(marshal),
+		Contract:    logEntry.Address.Hex(),
+	}
+	result := models.DB.Where("tx_hash=? and log_index=?", entry.TxHash, entry.LogIndex).FirstOrCreate(&entry)
+	if result.Error != nil {
+		return false, result.Error
+	}
+	return result.RowsAffected > 0, nil
+}
+
+func (l *listenerService) setSyncBlock(key string, block uint64) error {
+	state := models.SyncState{Name: key, BlockNumber: block}
+	return models.DB.Where("name=?", key).Assign(models.SyncState{BlockNumber: block}).FirstOrCreate(&state).Error
 }
 
 func (l *listenerService) getSyncBlock(key string) (uint64, error) {
