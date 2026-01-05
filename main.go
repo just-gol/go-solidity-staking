00cc938866a1ab175ce8f6b2d4f384eae62ee09c
aabb <809073023@qq.com>
2026-01-05 10:47:17 +0800

    feat: add main logic

--- main.go
+++ main.go
@@ -1,5 +1,11 @@
 package main
 
-func main() {
+import "go-solidity-staking/bootstrap"
 
+func main() {
+	app, err := bootstrap.NewApp()
+	if err != nil {
+		panic(err)
+	}
+	_ = app.Run(":8080")
 }
