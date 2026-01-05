0358ed7a84a42b67bbab28b2f492b6f1c7939d6a
aabb <809073023@qq.com>
2026-01-05 11:01:35 +0800

    feat(staking): add stake

---
+++ service/staking.go
@@ -0,0 +1,44 @@
+package service
+
+import (
+	"context"
+	"crypto/ecdsa"
+	staking "go-solidity-staking/gen"
+	"math/big"
+
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/ethclient"
+)
+
+type StakingService interface {
+	stake(ctx context.Context, contractAddress common.Address, privateKey *ecdsa.PrivateKey, amount uint64) (*types.Transaction, error)
+}
+
+type stakingService struct {
+	client *ethclient.Client
+}
+
+func NewStakingService(client *ethclient.Client) StakingService {
+	return &stakingService{client: client}
+}
+func (s *stakingService) stake(ctx context.Context, contractAddress common.Address, privateKey *ecdsa.PrivateKey, amount uint64) (*types.Transaction, error) {
+	client := s.client
+	newStaking, err := staking.NewStaking(contractAddress, client)
+	if err != nil {
+		return nil, err
+	}
+	chainID, err := client.ChainID(ctx)
+	if err != nil {
+		return nil, err
+	}
+	auth, err := bind.NewKeyedTransactorWithChainID(
+		privateKey,
+		chainID,
+	)
+	if err != nil {
+		return nil, err
+	}
+	return newStaking.Stake(auth, new(big.Int).SetUint64(amount))
+}
